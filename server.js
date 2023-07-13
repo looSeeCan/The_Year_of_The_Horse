7801e1835003827c27d0b053f8cf48e0d84a328a
Lucycan LY <95648904+looSeeCan@users.noreply.github.com>
2023-07-13 16:07:46 -0500

    ...

---
+++ server.js
@@ -0,0 +1,6 @@
+console.log("express...........................................");
+const express = require("express");
+console.log(express);
+console.log("cors......................................");
+const cors = require("cors");
+console.log(cors);
